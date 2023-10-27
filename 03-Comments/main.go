62e2d4853e024c69cc0a39db1807cd604440c116
madisec <madiisec@gmail.com>
2023-10-27 23:31:44 +0330

    Go comments

---
+++ 03-Comments/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	// This is a single-line commnet
+	fmt.Println("Single-line comment")
+
+	/*
+		This is a multi-line comment
+		Line1
+		Line2
+		Line3
+	*/
+	fmt.Println("Multi-line comment")
+}
